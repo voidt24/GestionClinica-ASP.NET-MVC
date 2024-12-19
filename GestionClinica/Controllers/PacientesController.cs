ebe8f3ea3486aaa8e279070c553b8cdda5ee6622
Andrey <devandrey98@gmail.com>
2024-12-19 12:44:25 -0600

    initial commit
    also added initial views and controllers

---
+++ GestionClinica/Controllers/PacientesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GestionClinica.Controllers
+{
+    public class PacientesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
